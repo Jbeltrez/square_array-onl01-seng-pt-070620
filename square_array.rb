86cbb089a34b77633adbd195d607fa401cf5150f
john beltrez <beltrezjohn@gmail.com>
2020-08-03 22:19:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
-def square_array(array)
-
- new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
+def square_array(numbers)
+  new_array = []
+  numbers.each do |num|
+  new_array << (num ** 2)
+  end
+  return new_array
 end
-
